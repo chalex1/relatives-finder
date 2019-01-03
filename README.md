2463c3ac504311a2a566af720fac66b5f60b23a5
achunin <aleksandr.chunin@t-systems.com>
2019-01-03 15:52:06 +0300

    init

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 endpoints
  - getAllTrees
  - findSimilarTrees
+
+the sequence finding method
+    making all possible string with some rules and, same 
+    do with tree in wich we want search
+    and after that do search strings in strings
---
+++ src/main/java/chalex/finder/relativesfinder/RelativesFinderApplication.java
@@ -0,0 +1,14 @@
+package chalex.finder.relativesfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelativesFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelativesFinderApplication.class, args);
+	}
+
+}
+
